879d6aed2a65e488e8957d8c22ceabc3cb778a04
Theo Manton <theo333@gmail.com>
2021-08-08 17:42:32 -0400

    feat: add infinite scroll

--- src/components/ListContainer.jsx
+++ src/components/ListContainer.jsx
@@ -4,19 +4,43 @@ import ListItem from './ListItem';
 import './ListContainer.css';
 
 const ListContainer = () => {
-	const [rList, setRList] = useState([]);
+	const [listItems, setListItems] = useState([]);
+	const [after, setAfter] = useState('');
+	const [count, setCount] = useState(0)
+	const [isLoading, setIsLoading] = useState(false);
 
+	// debugger;
 	useEffect(() => {
 		fetchData();
 	}, []);
 
+	useEffect(() => {
+		window.addEventListener('scroll', handleScrollEvent);
+		return () => {
+			window.removeEventListener('scroll', handleScrollEvent);
+		}
+	})
+
+	const handleScrollEvent = () => {
+		const {clientHeight, scrollTop, scrollHeight} = document.documentElement;
+		if (clientHeight + scrollTop >= scrollHeight) {
+			console.log('load more items... | after: ', after)
+			setIsLoading(true);
+			fetchData();
+		}
+	};
+
 	const getRedditList = async () => {
 		try {
-			const resp = await axios.get('https://api.reddit.com/r/aww/top.json');
+			console.log('getR count: ', count)
+			let paramsStr = `?limit=5`;
+			paramsStr += `&after=${after}`;
+			console.log('paramsStr: ', paramsStr)
+			const resp = await axios.get(`https://api.reddit.com/r/aww/top.json${paramsStr}`);
 			if (resp.status !== 200) {
-				console.log('there was an error');
+				console.error('there was an error');
 			} else {
-				// console.log(resp.data)
+				console.log('getRedditList data: ', resp.data.data)
 				return resp.data;
 			}
 		} catch (err) {
@@ -26,17 +50,32 @@ const ListContainer = () => {
 
 	const fetchData = async () => {
 		const list = await getRedditList();
-		console.log('after: ', list.data.after)
-		console.log('children: ', list.data.children)
-		setRList(list.data.children);
+		const items = list?.data?.children;
+		const itemCount = items?.length;
+		const _after = list?.data?.after;
+		console.log('itemCount: ', itemCount)
+		console.log('after: ', _after)
+		console.log('items: ', items)
+		console.log('count: ', count)
+		setListItems(prevState => {
+			return [...prevState, ...items];
+		});
+		setAfter(_after);
+		setCount(prevState => {
+			console.log('count prevState: ', prevState)
+			return prevState + itemCount;
+		});
 	}
 
 	return (
-		<ul className="reddit-list">
-			{rList && rList.map(item => {
-				return <ListItem item={item} key={item.data.id}/>
-			})}
-		</ul>
+		<div>
+			<ul className="reddit-list">
+				{listItems && listItems.map(item => {
+					return <ListItem item={item} key={item?.data?.id}/>
+				})}
+			</ul>
+			{isLoading && <p className="is-loading">Loading more items...</p>}
+		</div>
 	);
 };
 
