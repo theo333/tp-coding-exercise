97d826011fc121700953efda7398c7f80e429600
Theo Manton <theo333@gmail.com>
2021-08-08 09:52:57 -0400

    feat: display  data in list

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -7,7 +7,7 @@ const ListContainer = () => {
 
 	useEffect(() => {
 		fetchData();
-	}, [])
+	}, []);
 
 	const getRedditList = async () => {
 		try {
@@ -30,7 +30,21 @@ const ListContainer = () => {
 		setRList(list.data.children);
 	}
 
-	return <div>Hello</div>;
+	return (
+		<ul className="reddit-list">
+			{rList && rList.map(item => {
+				const {id, title, subreddit_name_prefixed, thumbnail, permalink} = item.data;
+				const baseUrl = "https://reddit.com/"
+				return (
+					<li className="reddit-list-item" key={id}>
+						<a href={baseUrl + permalink} alt="" target="_blank">{title}</a>
+						{subreddit_name_prefixed}
+						<img src={thumbnail} />
+					</li>
+				);
+			})}
+		</ul>
+	);
 };
 
 export default ListContainer;
