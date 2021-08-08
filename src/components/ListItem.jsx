7dde0d8aa88bded488ea045f0e1884be14185cce
Theo Manton <theo333@gmail.com>
2021-08-08 10:55:19 -0400

    style: list items

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import './ListItem.css';
 
 const ListItem = ({item}) => {
-	const {id, title, subreddit_name_prefixed, thumbnail, permalink} = item.data;
+	const {id, title, subreddit_name_prefixed, thumbnail,thumbnail_height, thumbnail_width, permalink} = item.data;
 	const baseUrl = "https://reddit.com/"
 	return (
 		<li className="reddit-list-item" key={id}>
-			<a href={baseUrl + permalink} alt="" target="_blank" rel="noreferrer">{title}</a>
-			{subreddit_name_prefixed}
-			<img src={thumbnail} />
+			<p><a href={baseUrl + permalink} target="_blank" rel="noreferrer">{title}</a></p>
+			<div className="reddit-list-item-right">
+				<p>{subreddit_name_prefixed}</p>
+				<img src={thumbnail} alt="" width={thumbnail_width} height={thumbnail_height}/>
+			</div>
 		</li>
 	);
 };
